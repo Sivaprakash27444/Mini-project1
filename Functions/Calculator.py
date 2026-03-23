b9780bedc4efe5c1ac6422f5d13f6acb87ed3f1b
Sivaprakash <sivaprakash27444@gmail.com>
2026-03-23 10:05:57 +0530

    Initial commit

---
+++ Functions/Calculator.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
+def multiplication(a,b):
+    return a*b
+
+def division(a,b):
+    return a/b
+
+print("Addition: ",add(10,5))
+print("Subraction ",subtraction(10,5))
+print("multiplication: ",multiplication(10,5))
+print("Division: ",division(10,5))
